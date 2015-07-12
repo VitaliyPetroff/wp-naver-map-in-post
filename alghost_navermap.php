e6d3bd0e844bbc26decd6fff91c257e091d1a618
Alghost <Alghost@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-12 06:37:44 +0000

    Remove 'file_get_contents' function
    git-svn-id: https://plugins.svn.wordpress.org/wp-naver-map-in-post/trunk@1197305 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- alghost_navermap.php
+++ alghost_navermap.php
@@ -29,7 +29,19 @@ add_shortcode('navermap', 'alghost_add_naver_map');
 add_action('wp_ajax_alghost_get_locations_from_keyword', 'alghost_get_locations_from_keyword');
 add_action('wp_ajax_nopriv_alghost_get_locations_from_keyword', 'alghost_get_locations_from_keyword');
 add_action('admin_menu', 'alghost_navermap_admin_setup');
+function alghost_file_get_contents_curl($url) {
+    $ch = curl_init();
 
+    curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);       
+    $data = curl_exec($ch);
+    curl_close($ch);
+
+    return $data;
+}
 function alghost_get_locations_from_keyword(){
     $keyword = $_POST['keyword'];
     if (!$keyword)
@@ -42,7 +54,7 @@ function alghost_get_locations_from_keyword(){
 
     $url = "http://openapi.naver.com/search?key=".$api_key."&query=".$keyword."&target=local&start=1&display=10";
 
-    $response = file_get_contents($url);
+    $response = alghost_file_get_contents_curl($url);
     $object = simplexml_load_string($response);
 
     $channel = $object->channel;
@@ -84,13 +96,54 @@ function alghost_add_naver_map($atts){
     $all_script = file_get_contents(plugin_dir_path(__FILE__).".SCRIPT_LAYOUT");
     $api_key = esc_attr(get_option('alghost-navermap-map-key'));
     $api_key = trim($api_key);
-    
-    $all_script = str_replace("__INPUT__PLUGINPATH", plugins_url(), $all_script);
-    $all_script = str_replace("__INPUT__APIKEY", $api_key, $all_script);
-    $all_script = str_replace("__INPUT__X", $location['mapx'], $all_script);
-    $all_script = str_replace("__INPUT__Y", $location['mapy'], $all_script);
-    $location['title'] = strip_tags($location['title']);
-    $all_script = str_replace("__INPUT__TITLE", $location['title'], $all_script);
+    $all_script = "<script type=\"text/javascript\" src=\"http://openapi.map.naver.com/openapi/naverMap.naver?ver=2.0&key=".$api_key."\"></script>";
+    $all_script .= "<script type=\"text/javascript\" src=\"".plugins_url()."/wp-naver-map-in-post/conv_loc/lib/geocent.js\"></script>";
+    $all_script .= "<script type=\"text/javascript\" src=\"".plugins_url()."/wp-naver-map-in-post/conv_loc/lib/tmerc.js\"></script>";
+    $all_script .= "<script type=\"text/javascript\" src=\"".plugins_url()."/wp-naver-map-in-post/conv_loc/lib/cscs.js\"></script>";
+    $all_script .= "<SCRIPT type=\"text/javascript\" src=\"".plugins_url()."/wp-naver-map-in-post/conv_loc/lib/defs/GOOGLE_WGS84.js\"></SCRIPT>";
+    $all_script .= "<SCRIPT type=\"text/javascript\" src=\"".plugins_url()."/wp-naver-map-in-post/conv_loc/lib/defs/TM128_katech_3param.js\"></SCRIPT>";
+    $all_script .= "<p style=\"margin-left:15px; font-size:0.9em\"><b>".strip_tags($location['title'])."지도</b></p>";
+    $all_script .= "<p id=\"alghostMap\" style=\"border:1px solid #000; max-width:400px; height:300px; margin:15px;\"></p>";
+    $all_script .= "<p style=\"margin-left:15px; font-size:0.75em;\"><a id=\"alghostMobileLink\">네이버지도앱으로 연결</a></p>";
+    $all_script .= "<script type=\"text/javascript\">";
+    $all_script .= "var TM128 = new CS(csList.TM128_katech_3param);";
+    $all_script .= "var WGS84 = new CS(csList.GOOGLE_WGS84);";
+    $all_script .= "var convPtr = new PT(".$location['mapx'].",".$location['mapy'].");";
+    $all_script .= "cs_transform(TM128, WGS84, convPtr);";
+    $all_script .= "var objCenterPtr = new nhn.api.map.LatLng(convPtr.y, convPtr.x);";
+    $all_script .= "var defaultLevel = 11;";
+    $all_script .= "var objMap = new nhn.api.map.Map(document.getElementById(\"alghostMap\"), {";
+    $all_script .= "        point: objCenterPtr,";
+    $all_script .= "       zoom: defaultLevel,";
+    $all_script .= "       enableWheelZoom: true,";
+    $all_script .= "       enableDragPan: true,";
+    $all_script .= "       enableDblClickZoom: false,";
+    $all_script .= "       mapMode: 0,";
+    $all_script .= "       activateTrafficMap: false,";
+    $all_script .= "       activateBicycleMap: false,";
+    $all_script .= "       minMaxLevel: [1,14],";
+    $all_script .= "       size: new nhn.api.map.Size(300, 300)";
+    $all_script .= "   });";
+    $all_script .= "var infoWindow = new nhn.api.map.InfoWindow();";
+    $all_script .= "   objMap.addOverlay(infoWindow);";
+    $all_script .= "   var objMapSlider = new nhn.api.map.ZoomControl();";
+    $all_script .= "   objMap.addControl(objMapSlider);";
+    $all_script .= "   objMapSlider.setPosition({";
+    $all_script .= "       top: 10,";
+    $all_script .= "       left: 10";
+    $all_script .= "   });";
+    $all_script .= "   var objIconSize = new nhn.api.map.Size(28, 37);";
+    $all_script .= "   var objIconOffset = new nhn.api.map.Size(14,37);";
+    $all_script .= "   var objMarkerIcon = new nhn.api.map.Icon(\"http://static.naver.com/maps2/icons/pin_spot2.png\", objIconSize, objIconOffset);";
+    $all_script .= "   var objMarker = new nhn.api.map.Marker(objMarkerIcon, {title: \"".strip_tags($location['title'])."\"});";
+    $all_script .= "   objMarker.setPoint(objCenterPtr);";
+    $all_script .= "   objMap.addOverlay(objMarker);";
+    $all_script .= "   var objMarkerLabel = new nhn.api.map.MarkerLabel();";
+    $all_script .= "   objMap.addOverlay(objMarkerLabel);";
+    $all_script .= "   objMarkerLabel.setVisible(true, objMarker);";
+    $all_script .= "   var mobile_url = \"navermaps://?menu=location&lat=\"+convPtr.y+\"&lng=\"+convPtr.x+\"&title=".strip_tags($location['title'])."&mLevel=11\";";
+    $all_script .= "   document.getElementById(\"alghostMobileLink\").href = mobile_url;";
+    $all_script .= "   </script>";
 
     return strval($all_script);
 }
